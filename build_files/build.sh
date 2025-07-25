d0218b32806058f13dea069682466708d63131dd
ECon65 <missbach.ales@gmail.com>
2025-07-25 22:14:54 +0200

    hyprland using ml4w dotfiles/fix 2: skip unavailable packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -25,7 +25,7 @@ dnf5 copr enable --assumeyes peterwu/rendezvous
 dnf5 copr enable --assumeyes tofik/nwg-shell
 dnf5 copr enable --assumeyes erikreider/SwayNotificationCenter
 
-dnf5 install --assumeyes wget unzip gum rsync git figlet xdg-user-dirs hyprland hyprpaper hyprlock hypridle hyprpicker noto-fonts google-noto-emoji-fonts google-noto-sans-cjk-fonts xdg-desktop-portal-hyprland libnotify kitty qt5-qtwayland qt6-qtwayland uwsm fastfetch xdg-desktop-portal-gtk python-pip python3-gobject tumbler brightnessctl nm-connection-editor network-manager-applet gtk4 libadwaita fuse nautilus ImageMagick jq xclip kitty neovim htop blueman waypaper grim slurp cliphist nwg-look qt6ct waybar rofi-wayland zsh fzf pavucontrol papirus-icon-theme papirus-icon-theme-dark breeze flatpak SwayNotificationCenter gvfs wlogout bibata-cursor-themes fontawesome-fonts dejavu-fonts-all NetworkManager-tui nwg-dock-hyprland vlc
+dnf5 install --assumeyes --skip-unavailable wget unzip gum rsync git figlet xdg-user-dirs hyprland hyprpaper hyprlock hypridle hyprpicker noto-fonts google-noto-emoji-fonts google-noto-sans-cjk-fonts xdg-desktop-portal-hyprland libnotify kitty qt5-qtwayland qt6-qtwayland uwsm fastfetch xdg-desktop-portal-gtk python-pip python3-gobject tumbler brightnessctl nm-connection-editor network-manager-applet gtk4 libadwaita fuse nautilus ImageMagick jq xclip kitty neovim htop blueman waypaper grim slurp cliphist nwg-look qt6ct waybar rofi-wayland zsh fzf pavucontrol papirus-icon-theme papirus-icon-theme-dark breeze flatpak SwayNotificationCenter gvfs wlogout bibata-cursor-themes fontawesome-fonts dejavu-fonts-all NetworkManager-tui nwg-dock-hyprland vlc
 
 dnf5 copr disable --assumeyes solopasha/hyprland
 dnf5 copr disable --assumeyes peterwu/rendezvous
